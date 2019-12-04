805685bc25ab9fbddb365469f5eab3edeb5e1cfa
Eric Lauret <eric.lauret@live.com>
2019-12-04 09:57:28 +0100

    Solved part I and II of DayFour

--- DayFour/Program.cs
+++ DayFour/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using MoreLinq;
 
 namespace DayFour
 {
@@ -7,19 +9,34 @@ namespace DayFour
     {
         static void Main(string[] args)
         {
-            var path = $"Resources/input.txt";
+            Console.WriteLine($"What is the lowest boundary of the range?");
+            var minimum = int.Parse(Console.ReadLine());
 
-            try
-            {
-                if (!File.Exists(path)) throw new FileNotFoundException();
-                var lines = File.ReadAllLines(path);
+            Console.WriteLine($"What is the highest boundary of the range?");
+            var maximum = int.Parse(Console.ReadLine());
 
-                if (lines.Length == 0) throw new Exception(@"File is empty.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+
+            // Count of numbers within range with increasing digits
+            var count = Enumerable.Range(minimum, maximum - minimum + 1)
+                .Where(n => n.ToString().Window(2).All(w => w[0] <= w[1])).Count();
+            Console.WriteLine(count);
+
+            // Count of numbers within range with strictly increasing digits
+            count = Enumerable.Range(minimum, maximum - minimum + 1)
+                .Where(n => n.ToString().Window(2).All(w => w[0] < w[1])).Count();
+            Console.WriteLine(count);
+
+            // Count of numbers within range with increasing digits and at least a pair of adjacent and identical digits
+            count = Enumerable.Range(minimum, maximum - minimum + 1)
+                .Where(n => n.ToString().Window(2).All(w => w[0] <= w[1]))
+                .Where(n => n.ToString().Window(2).Any(w => w[0] == w[1])).Count();
+            Console.WriteLine(count);
+
+            // Count of numbers within range with increasing digits and at least an exact pair of adjacent and identical digits
+            count = Enumerable.Range(minimum, maximum - minimum + 1)
+                .Where(n => n.ToString().Window(2).All(w => w[0] <= w[1]))
+                .Where(n => n.ToString().GroupAdjacent(d => d).Any(g => g.Count() == 2)).Count();
+            Console.WriteLine(count);
         }
     }
 }
