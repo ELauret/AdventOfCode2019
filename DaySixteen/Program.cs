61b063cdcf8f1b09899ea115bc3b87ef08cfc28b
Eric Lauret <eric.lauret@live.com>
2019-12-16 23:11:11 +0100

    SOlved part II of Day Sixteen

--- DaySixteen/Program.cs
+++ DaySixteen/Program.cs
@@ -18,6 +18,9 @@ namespace DaySixteen
                 var lines = FileReader.ReadAllLines(path);
 
                 var inputArray = lines.ElementAt(0).ToCharArray().Select(c => double.Parse(c.ToString()));
+
+                // Part I
+
                 var inputVector = Vector<double>.Build.DenseOfEnumerable(inputArray);
                 var dimension = inputArray.Count();
 
@@ -39,13 +42,36 @@ namespace DaySixteen
                     }
                 }
 
+                //for (int i = 0; i < dimension; i++)
+                //{
+                //    Console.WriteLine(string.Join(' ', matrix.ToRowArrays()[i]
+                //        .Select(v => (v == 0 ? "'" : ((int)v).ToString()).PadLeft(3))));
+                //}
+
                 for (int i = 0; i < 100; i++)
                 {
                     var outputVector = matrix * inputVector;
-                    inputVector = Vector<double>.Build.DenseOfEnumerable(
-                        outputVector.ToArray().Select(c => double.Parse(((int)c).ToString().ToCharArray().Last().ToString())));
-                    Console.WriteLine(string.Join(null,inputVector.ToArray().Take(8)));
+                    inputVector = Vector<double>.Build.DenseOfEnumerable(outputVector.ToArray().Select(c => Math.Abs(c % 10)));
                 }
+                Console.WriteLine(string.Join(null, inputVector.ToArray().Take(8)));
+
+                // Part II
+
+                var offset = int.Parse(string.Join(null,inputArray.Take(7)));
+
+                var inputList = new List<int>();
+                for (int i = 0; i < 10000; i++) inputList.AddRange(inputArray.Select(ipt => (int)ipt));
+
+                inputList = inputList.Skip(offset).ToList();
+
+                for (int i = 0; i < 100; i++)
+                {
+                    for (int j = inputList.Count - 1; j >= 0; j--)
+                    {
+                        inputList[j] = Math.Abs(inputList[j] + (j < inputList.Count - 1 ? inputList[j+1] : 0)) % 10;
+                    }
+                }
+                Console.WriteLine(string.Join(null, inputList.Take(8)));
             }
             catch (IOException ex)
             {
